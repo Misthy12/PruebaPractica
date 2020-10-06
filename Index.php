a5e4bd0413be05ed0d93ecc69aa2b9f0ee70f211
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-06 14:00:23 -0600

    Cambios en consultas

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Login Cuponera</title>
+  <title>Login Evaluaciones KANF</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -26,27 +26,27 @@ session_start();
   <!-- /.login-logo -->
   <div class="card">
         <div class="login-logo card-header">
-          <a href="#" class="text-uppercase font-weight-bold "><em>La Cuponera</em></a>
+          <a href="#" class="text-uppercase font-weight-bold "><em>Evaluaciones KANF!</em></a>
         </div>
       <div class="card-body login-card-body">
         <div class="text-center"><i class="fas fa-user-circle text-center fa-4x"></i></div>
         <p class="login-box-msg h5">Iniciar Sesión</p>
         <ul class="nav nav-tabs" id="myTab" role="tablist">
     <li class="nav-item" role="presentation">
-      <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Cliente</a>
+      <a class="nav-link active" id="Estudiante-tab" data-toggle="tab" href="#Estudiante" role="tab" aria-controls="Estudiante" aria-selected="true">Estudiante</a>
     </li>
     <li class="nav-item" role="presentation">
-      <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Empresa</a>
+      <a class="nav-link" id="docente-tab" data-toggle="tab" href="#docente" role="tab" aria-controls="docente" aria-selected="false">Docente</a>
     </li>
     <li class="nav-item" role="presentation">
-      <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Administrador</a>
+      <a class="nav-link" id="admin-tab" data-toggle="tab" href="#admin" role="tab" aria-controls="admin" aria-selected="false">Administrador</a>
     </li>
   </ul>
   <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    <div class="tab-pane fade show active" id="Estudiante" role="tabpanel" aria-labelledby="Estudiante-tab">
     <form action="" method="post">
           <div class="input-group mb-3">
-            <input type="email" id="email" name="email" class="form-control" placeholder="Email">
+            <input type="text" id="email" name="email" class="form-control" placeholder="Usuario">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -71,16 +71,16 @@ session_start();
             </div>
             <!-- /.col -->
             <div class="col-4">
-              <button type="submit" id="btnCliente" name="btnCliente" class="btn btn-primary btn-block">INICIAR</button>
+              <button type="submit" id="btnEstudiante" name="btnEstudiante" class="btn btn-primary btn-block">INICIAR</button>
             </div>
             <!-- /.col -->
           </div>
         </form>
     </div>
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+    <div class="tab-pane fade" id="docente" role="tabpanel" aria-labelledby="docente-tab">
     <form action="" method="post">
     <div class="input-group mb-3">
-            <input type="email" id="email" name="email" class="form-control" placeholder="Email">
+            <input type="text" id="email" name="email" class="form-control" placeholder="Usuario">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -100,16 +100,16 @@ session_start();
             </div>
             <!-- /.col -->
             <div class="col-4">
-              <button type="submit" id="btnEmpresa" name="btnEmpresa" class="btn btn-primary btn-block">INICIAR</button>
+              <button type="submit" id="btnDocente" name="btnDocente" class="btn btn-primary btn-block">INICIAR</button>
             </div>
             <!-- /.col -->
           </div>
         </form>
     </div>
-    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+    <div class="tab-pane fade" id="admin" role="tabpanel" aria-labelledby="admin-tab">
     <form action="" method="post">
     <div class="input-group mb-3">
-            <input type="email" id="email" name="email" class="form-control" placeholder="Email">
+            <input type="text" id="email" name="email" class="form-control" placeholder="Usuario">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -158,20 +158,22 @@ session_start();
 <?php
 include("Share/conexion.php"); //incluimos el archivo que se conecta a la base de datos
 $conn=OpenCon();
-if (isset($_POST['btnCliente'])) { //comprobamos si se envían variables desde el form por el método POST
+if (isset($_POST['btnEstudiante'])) { //comprobamos si se envían variables desde el form por el método POST
   $email=$_POST["email"]; //Capturamos lo digitado por el usuario en la caja de texto de nombre usuario
   $clave=$_POST["password"];//clave encriptada
-  $stmt = $conn->prepare("SELECT *FROM tblclientes WHERE correoCliente=:email"); 
+  $stmt = $conn->prepare("SELECT a.idAlumno, a.alumnoNombre a.alumnoApellido, a.clave, u.usuario, u.clave, u.idRol  FROM tblAlumnos a
+                          INNER JOIN tblUsuarios u ON a.IdUsuario = u.idUsuario WHERE u.usuario=:email"); 
   $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
   //$stmt->bindParam("clave", $clave,PDO::PARAM_STR) ;
   $stmt->execute();
   $count=$stmt->rowCount();
   $data=$stmt->fetch(PDO::FETCH_OBJ);
-  if($count>0 && password_verify($clave,$data->password) ){
+  if($count>0 && password_verify($clave,$data->password)&& $data->idRol=3 ){
+    $alumno= $data->nombreAlumno."".$data->apellidoAlumno;
     //variables allevar
-    $_SESSION['id']=$data->idCliente; // Storing user session value
-    $_SESSION["nombre"]=$data->nombresCliente;
-    $_SESSION["login"]="Cliente";//identificar la sesion
+    $_SESSION['id']=$data->idAlumno; // Storing user session value
+    $_SESSION["nombre"]=$alumno;
+    $_SESSION["login"]="Estudiante";//identificar la sesion
     print "<script> window.location = './Vistas/Index/IndexClientes.php';</script>";
   }
   else
@@ -181,28 +183,30 @@ if (isset($_POST['btnCliente'])) { //comprobamos si se envían variables desde e
     Swal.fire({
       icon: 'error',
       title: 'ERROR!',
-      text: 'Datos Clientes Incorrectos!',
+      text: 'Datos Alumno Incorrectos!',
     })
     </script>
     <?php
   } 
 }
-else if (isset($_POST['btnEmpresa'])) { //comprobamos si se envían variables desde el form por el método POST
+else if (isset($_POST['btnDocente'])) { //comprobamos si se envían variables desde el form por el método POST
   $email=$_POST["email"]; //Capturamos lo digitado por el usuario en la caja de texto de nombre usuario
   $clave=$_POST["password"];//clave encriptada
   //$hash_password= hash('sha256', $password); //Password encryption 
-  $stmt = $conn->prepare("SELECT *FROM tblsucursales WHERE correo=:email"); 
+  $stmt = $conn->prepare("SELECT d.idDocente, d.docenteNombre, d.docenteApellido, u.usuario, u.clave, u.idRol  FROM tblDocentes d
+                          INNER JOIN tblUsuarios u ON d.IdUsuario = u.idUsuario WHERE u.usuario=:email"); 
   $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
   //$stmt->bindParam("clave", $clave,PDO::PARAM_STR) ;
   $stmt->execute();
   $count=$stmt->rowCount();
   $data=$stmt->fetch(PDO::FETCH_OBJ);
-  if($count>0 && password_verify($clave,$data->password) ){
+  if($count>0 && password_verify($clave,$data->clave)&& $data->idRol=2 ){
+    $docente=$data->nombreDocente." ".$data->apellidoDocente;
     //variables allevar
-    $_SESSION['id']=$data->idSucursal; // Storing user session value
-    $_SESSION["nombre"]=$data->nombreSucursal;
+    $_SESSION['id']=$data->idDocente; // Storing user session value
+    $_SESSION["nombre"]=$docente;
     //$_SESSION["sucursal"]=$data->nombreSucursal;
-    $_SESSION["login"]="Sucursal";//identificar la sesion
+    $_SESSION["login"]="Docente";//identificar la sesion
     print "<script> window.location = './Vistas/Index/IndexEmpresas.php';</script>";
   }
   else
@@ -212,7 +216,7 @@ else if (isset($_POST['btnEmpresa'])) { //comprobamos si se envían variables de
     Swal.fire({
       icon: 'error',
       title: 'ERROR!',
-      text: 'Datos de Empresa Incorrectos!',
+      text: 'Datos de Docente Incorrectos!',
     })
     </script>
     <?php
@@ -223,13 +227,13 @@ else if (isset($_POST['btnAdmin'])) { //comprobamos si se envían variables desd
   $email=$_POST["email"]; //Capturamos lo digitado por el usuario en la caja de texto de nombre usuario
   $clave=$_POST["password"];//clave encriptada
   //$hash_password= hash('sha256', $password); //Password encryption 
-  $stmt = $conn->prepare("SELECT *FROM tblUsuarios WHERE email=:email"); 
+  $stmt = $conn->prepare("SELECT * FROM tblUsuarios WHERE u.usuario=:email"); 
   $stmt->bindParam("email", $email,PDO::PARAM_STR) ;
   //$stmt->bindParam("clave", $clave,PDO::PARAM_STR) ;
   $stmt->execute();
   $count=$stmt->rowCount();
   $data=$stmt->fetch(PDO::FETCH_OBJ);
-  if($count>0 && password_verify($clave,$data->password) ){
+  if($count>0 && password_verify($clave,$data->password) && $data->idRol=1 ){
     //variables allevar
     $_SESSION['id']=$data->idUsuario; // Storing user session value
     $_SESSION["nombre"]=$data->nombreUsuario;
