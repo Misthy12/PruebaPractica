71c20fec3fe01b487321aea34766e0d1094d549e
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-07 16:07:34 -0600

    Cambios en disenio

--- Vistas/Evaluaciones/agregar_evaluacion.php
+++ Vistas/Evaluaciones/agregar_evaluacion.php
@@ -1,6 +1,7 @@
 <?php
 include "../../Share/header.php";
-include "../../Share/conexion.php";
+include "../../Share/funciones.php";
+//include "../../Share/conexion.php";
 //consulta para roles
 $conn = OpenCon();
 $sqlDocentes = "SELECT * FROM tbldocentes";
@@ -24,7 +25,7 @@ CloseCon($conn);
                             <div class="form-group col-4">
                                 <label class="control-label col-md-3">Codigo</label>
                                 <div class="col-md-9">
-                                    <input class="form-control" type="text" name="codigo" id="codigo" value="" placeholder="Ingrese un Codigo" required />
+                                    <input class="form-control" type="text" name="codigo" id="codigo" value="<?php echo generarCodigo(4) ?>" placeholder="Ingrese un Codigo" required />
                                 </div>
                             </div>
                             <div class="form-group col-4">
@@ -39,7 +40,7 @@ CloseCon($conn);
                                     <select name="idDocente" id="idDocente" type="text" class="form-control" required>
                                         <?php
                                         foreach ($conn->query($sqlDocentes) as $valord) {
-                                            echo "<option value='" . $valord["idDocente"] . "'>" . $valord["docenteNombre"] . "</option>";
+                                            echo "<option value='" . $valord["idDocente"] . "'>" . $valord["docenteNombre"]." ". $valord["docenteApellido"]. "</option>";
                                         }
                                         ?>
                                     </select>
@@ -60,13 +61,13 @@ CloseCon($conn);
             <div class="card">
                 <div class="card-header bg-primary">
                     <h4 class="text-center">Preguntas</h4>
-                    <button title="Agregar Pregunta" class="btn btn-info" name="btnAgregar" id="btnAgregar"><i class="fa fa-plus"></i></button>
+                    <button title="Agregar Pregunta" class="btn btn-warning" name="btnAgregar" id="btnAgregar"><i class="fa fa-plus"></i></button>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="form-group col-3">
-                            <label class="control-label col-md-3" for="idTipo">Tipo</label>
-                            <select name="idTipo" id="idTipo" type="text" class="form-control" required>
+                    <div class="row col-12">
+                        <div class="form-group col-md-3 col-sm-12">
+                            <label class="control-label col-md-3 col-sm-12" for="idTipo">Tipo</label>
+                            <select name="idTipo" id="idTipo" type="text" class="form-control col-sm-12" required>
                                 <option value="">.:Seleccione:.</option>
                                 <?php
                                 foreach ($conn->query($sqlTipo) as $valorp) {
@@ -75,32 +76,34 @@ CloseCon($conn);
                                 ?>
                             </select>
                         </div>
-                        <div class="form-group col-9">
-                            <label class="control-label col-md-3" for="pregunta">Pregunta</label>
-                            <div class="col-md-9">
+                        <div class="form-group col-md-9 col-sm-12">
+                            <label class="control-label col-md-3 col-sm-12" for="pregunta">Pregunta</label>
+                            <div class="col-md-9 col-sm-12">
                                 <input class="form-control" type="text" name="pregunta" id="pregunta" value="" placeholder="Ingrese la pregunta" required />
                             </div>
 
                         </div>
                     </div>
-                </div>
+                
 
-                <div id="FormTrueFalse" style="display: none;">
-                    <label for="">Repuesta</label>
-                    <select name="Respuesta" id="Respuesta">
-                        <option value="">.:Seleccione:.</option>
-                        <option value="1">Verdadero</option>
-                        <option value="0">Falso</option>
-                    </select>
+                    <div id="FormTrueFalse" style="display: none;" class="form-group  col-12">
+                            <label for="Respuesta"  class="col-md-3 col-sm-12">Respuesta</label>
+                            <select name="Respuesta" id="Respuesta" class="form-control col-md-4 col-sm-12">
+                                <option value="">.:Seleccione:.</option>
+                                <option value="1">Verdadero</option>
+                                <option value="0">Falso</option>
+                            </select>
+                    </div>
+                    
+                        
+                    <div id="FormMultiple" style="display: none;" class="row form-group  col-12">
+                        <label for="RespuestaV" class="col-md-12">Respuesta</label>
+                        <input class="form-control col-md-12" type="text" name="RespuestaV" id="RespuestaV" value="" placeholder="Ingrese la Respuesta Correcta" />
+                        <input class="form-control col-md-12" type="text" name="RespuestaF1" id="RespuestaF1" value="" placeholder="Ingrese la Respuesta Incorrecta" />
+                        <input class="form-control col-md-12" type="text" name="RespuestaF2" id="RespuestaF2" value="" placeholder="Ingrese la Respuesta Incorrecta" />
+                        <input class="form-control col-md-12" type="text" name="RespuestaF3" id="RespuestaF3" value="" placeholder="Ingrese la Respuesta Incorrecta" />
+                    </div>
                 </div>
-                <div id="FormMultiple" style="display: none;>
-                                <label for=" RespuestaV">Repuesta</label>
-                    <input class="form-control" type="text" name="RespuestaV" id="RespuestaV" value="" placeholder="Ingrese la Respuesta Correcta" />
-                    <input class="form-control" type="text" name="RespuestaF1" id="RespuestaF1" value="" placeholder="Ingrese la Respuesta Incorrecta" />
-                    <input class="form-control" type="text" name="RespuestaF2" id="RespuestaF2" value="" placeholder="Ingrese la Respuesta Incorrecta" />
-                    <input class="form-control" type="text" name="RespuestaF3" id="RespuestaF3" value="" placeholder="Ingrese la Respuesta Incorrecta" />
-                </div>
-
             </div>
         </div>
     </div>
@@ -108,43 +111,44 @@ CloseCon($conn);
         <div class="card">
             <div class="card-header bg-primary">
                 <h4 class="text-center">Detalles Evaluacion</h4>
-                <div class=" col-sm-offset-8 col-sm-4" align="right">
+                <div class="offset-md-10 col-md-2" >
                     <button title="Guardar Evaluacion" class="btn btn-success" name="btnGuardar" id="btnGuardar"> <i class="fas fa-save"></i></button>
                     <a href="listado_evaluaciones.php" title="Cancelar" class="btn btn-default"><i class="fas fa-times"></i></a>
                 </div>
             </div>
-            <div class="card-body" style="overflow-x:scroll">
+            <div class="card-body " style="overflow-x:scroll">
                 <!-- Date -->
-                <table id="TablaCL" class="table table-bordered table-responsive">
-                    <thead>
-                        <tr>
-                            <th>N</th>
-                            <th>Tipo</th>
-                            <th>Pregunta</th>
-                            <th>Respuesta</th>
-                            <th hidden>Incorrecta1</th>
-                            <th hidden>Incorrecta2</th>
-                            <th hidden>Incorrecta3</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody id="fila">
+                <div class="table-responsive">
+                    <table id="TablaCL" class="table table-bordered ">
+                        <thead>
+                            <tr>
+                                <th>N</th>
+                                <th>Tipo</th>
+                                <th>Pregunta</th>
+                                <th>Respuesta</th>
+                                <th hidden>Incorrecta1</th>
+                                <th hidden>Incorrecta2</th>
+                                <th hidden>Incorrecta3</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody id="fila">
 
-                        <tr hidden>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <tr hidden>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
 
-                        </tr>
-                    </tbody>
+                            </tr>
+                        </tbody>
 
-                </table>
-
+                    </table>
+                </div>
             </div>
         </div>
     </div>
